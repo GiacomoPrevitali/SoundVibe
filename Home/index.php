9fd39cd35dd9cd47e3380e5c13b1bf1cfa393f81
Giacomo Previtali <giacomoprevitali26@gmail.com>
2023-02-13 09:54:44 +0100

    Creazione Home page

--- Home/index.php
+++ Home/index.php
@@ -7,6 +7,29 @@
         <link rel="stylesheet" href="style.css" />
     </head>
     <body>
-   <h1>Home</h1>
+    <div class="sidebar">
+        <a href="account.php">
+            <div class="element">
+                <img src="./Foto/Account_Icon.png"  class="Image">
+                <p class="Name"> Nome Account</p>
+            </div>
+        </a>
+        <br>
+        <a href="libreria.php">
+        <div class="element">
+                <img src="./Foto/Library_Icon.png"  class="Image1">
+                <p class="Name"> Libreria</p>
+        </div>
+        </a>
+                <div class="logout">
+                    <img src="./Foto/Exit_Icon.png"  class="Image1">
+                    <p class="Name"> Logout</p>
+                </div>
+        </div>
+    </div>
+        <div class="square">
+
+        </div>
+    
     </body>
 </html>
