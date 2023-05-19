648ad53d2729cb6ab6db7e3192d16a4902516abf
Giacomo Previtali <giacomoprevitali26@gmail.com>
2023-05-19 16:26:17 +0200

    add

--- soundvibe .sql
+++ soundvibe .sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 17, 2023 alle 17:26
+-- Creato il: Mag 19, 2023 alle 10:26
 -- Versione del server: 10.4.28-MariaDB
 -- Versione PHP: 8.2.4
 
@@ -24,32 +24,23 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `38_0`
+-- Struttura della tabella `45_0`
 --
 
-CREATE TABLE `38_0` (
+CREATE TABLE `45_0` (
   `Id` int(11) NOT NULL,
   `Id_Playlist` int(11) NOT NULL,
   `Id_Song` int(11) NOT NULL,
   `Data_Aggiunta` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dump dei dati per la tabella `38_0`
---
-
-INSERT INTO `38_0` (`Id`, `Id_Playlist`, `Id_Song`, `Data_Aggiunta`) VALUES
-(1, 1, 1, '2023-05-16'),
-(3, 1, 2, '2023-05-17'),
-(4, 2, 2, '2023-05-17');
-
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `38_2`
+-- Struttura della tabella `45_2`
 --
 
-CREATE TABLE `38_2` (
+CREATE TABLE `45_2` (
   `Id` int(11) NOT NULL,
   `Titolo` varchar(255) NOT NULL,
   `Immagine` varchar(255) NOT NULL,
@@ -58,12 +49,58 @@ CREATE TABLE `38_2` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dump dei dati per la tabella `38_2`
+-- Dump dei dati per la tabella `45_2`
 --
 
-INSERT INTO `38_2` (`Id`, `Titolo`, `Immagine`, `Descrizione`, `Autore`) VALUES
-(1, 'Chill', 'Immagine', 'Playlist con canzoni tranquille', 'Giacomo Previtali'),
-(2, 'Workout', 'Immagine', 'Allenamento', 'Giacomo Previtali');
+INSERT INTO `45_2` (`Id`, `Titolo`, `Immagine`, `Descrizione`, `Autore`) VALUES
+(1, 'Preferiti', ' ', ' ', ' ');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `46_0`
+--
+
+CREATE TABLE `46_0` (
+  `Id` int(11) NOT NULL,
+  `Id_Playlist` int(11) NOT NULL,
+  `Id_Song` int(11) NOT NULL,
+  `Data_Aggiunta` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `46_0`
+--
+
+INSERT INTO `46_0` (`Id`, `Id_Playlist`, `Id_Song`, `Data_Aggiunta`) VALUES
+(1, 1, 1, '0000-00-00'),
+(2, 2, 1, '0000-00-00'),
+(3, 1, 2, '0000-00-00'),
+(4, 1, 2, '0000-00-00'),
+(5, 2, 2, '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `46_2`
+--
+
+CREATE TABLE `46_2` (
+  `Id` int(11) NOT NULL,
+  `Titolo` varchar(255) NOT NULL,
+  `Immagine` varchar(255) NOT NULL,
+  `Descrizione` varchar(255) NOT NULL,
+  `Autore` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `46_2`
+--
+
+INSERT INTO `46_2` (`Id`, `Titolo`, `Immagine`, `Descrizione`, `Autore`) VALUES
+(1, 'Preferiti', ' ', ' ', ' '),
+(2, 'Chill', 'Foto', 'Bella Playlist', 'Giacomo Previtali'),
+(3, 'Workout', 'Foto', 'Allenamento', 'Giacomo Previtali');
 
 -- --------------------------------------------------------
 
@@ -122,24 +159,35 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`Id`, `Nome`, `Cognome`, `Data_Nascita`, `Codice_Fiscale`, `Mail`, `Pass`) VALUES
-(38, 'Giacomo', 'Previtali', '2004-01-26', 'PRVGCM26A04A794H', 'giacomoprevitali11@gmail.com', 'e2fc714c4727ee9395f324cd2e7f331f');
+(45, 'Marco', 'Salvi', '2004-06-04', 'SLVMRC04H04A794R', 'marcosalvi@gmail.com', 'e2fc714c4727ee9395f324cd2e7f331f'),
+(46, 'Giacomo', 'Previtali', '2004-01-26', 'PRVGCM26A04A794H', 'giacomoprevitali11@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055');
 
 --
 -- Indici per le tabelle scaricate
 --
 
 --
--- Indici per le tabelle `38_0`
+-- Indici per le tabelle `45_0`
 --
-ALTER TABLE `38_0`
-  ADD PRIMARY KEY (`Id`),
-  ADD KEY `Id_Playlist` (`Id_Playlist`),
-  ADD KEY `Id_Song` (`Id_Song`);
+ALTER TABLE `45_0`
+  ADD PRIMARY KEY (`Id`);
 
 --
--- Indici per le tabelle `38_2`
+-- Indici per le tabelle `45_2`
 --
-ALTER TABLE `38_2`
+ALTER TABLE `45_2`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indici per le tabelle `46_0`
+--
+ALTER TABLE `46_0`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indici per le tabelle `46_2`
+--
+ALTER TABLE `46_2`
   ADD PRIMARY KEY (`Id`);
 
 --
@@ -165,16 +213,28 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT per la tabella `38_0`
+-- AUTO_INCREMENT per la tabella `45_0`
 --
-ALTER TABLE `38_0`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `45_0`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT per la tabella `38_2`
+-- AUTO_INCREMENT per la tabella `45_2`
 --
-ALTER TABLE `38_2`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+ALTER TABLE `45_2`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT per la tabella `46_0`
+--
+ALTER TABLE `46_0`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT per la tabella `46_2`
+--
+ALTER TABLE `46_2`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT per la tabella `audios`
@@ -192,18 +252,7 @@ ALTER TABLE `song`
 -- AUTO_INCREMENT per la tabella `users`
 --
 ALTER TABLE `users`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
-
---
--- Limiti per le tabelle scaricate
---
-
---
--- Limiti per la tabella `38_0`
---
-ALTER TABLE `38_0`
-  ADD CONSTRAINT `Id_Playlist` FOREIGN KEY (`Id_Playlist`) REFERENCES `38_2` (`Id`),
-  ADD CONSTRAINT `Id_Song` FOREIGN KEY (`Id_Song`) REFERENCES `song` (`Id`);
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
